847b402c2f22071d61347f8c6963f0accf130e8b
Gigiborlan <gigiborlan@gmail.com>
2020-07-25 17:27:38 -0300

    dummy serial read

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Raspberry
+
+## How to configure
+
+### Module get_service_data.py
+
+1. Crontab -e
+2. */20 * * * * python3 /home/pi/Documents/Smart-Plantation/Raspberry/get_service_data.py
+   0 * * * * python3 /home/pi/Documents/Smart-Plantation/Raspberry/post_data.py
+
--- post_data.py
+++ post_data.py
@@ -1,32 +1,43 @@
 import serial
 import requests
 from datetime import datetime
+from time import sleep
+
+def request_data_from_serial():
+    if now.hour == 16:
+        print('Water and retrieve data')
+        serial_interface.write(b'b')
+    else:
+        print('Retrieve data')
+        serial_interface.write(b'a')
 
 temperature = None
 moisture = None
 watering = None
 
-serial_interface = serial.Serial("/dev/ttyUSB0",9600)
-serial_interface.baudrate = 9600
-if datetime.now().hour == 16:
-    print('regar')
-    serial_interface.write(b'b')
-else:
-    print('n regar')
-    serial_interface.write(b'a')
+LOG_PATH = '/home/pi/Documents/Smart-Plantation/Raspberry/logs/log_post.txt'
+APP_URL = 'https://smart-plantation.herokuapp.com/data'
 
-serial_data = serial_interface.readline().decode().strip().split()
-print(serial_data)
-
-temperature, moisture, watering = serial_data
-print("Temp: " + temperature)
-print("Moist: " + moisture)
-print("Watering: " + watering)
-        
-r = requests.post('https://smart-plantation.herokuapp.com/data',params = {'temperature':temperature,'moisture':moisture,'watering':watering})
-
-with open('/home/pi/Documents/Smart-Plantation/Raspberry/logs/log_post.txt','a') as log:
-    log.write(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' Temp: ' + str(temperature) + ' Moist: ' + moisture + ' Water: ' + watering + ' Request Status: ' + str(r.status_code) + ' - ' + r.reason + '\n')
+serial_interface = serial.Serial("/dev/ttyUSB0",9600, timeout=3)
+now = datetime.now()
+timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
     
-#r = requests.post('http://0.0.0.0:5000/data',params = {'temperature':temperature,'moisture':moisture,'watering':watering})
-#print(r.status_code)
+try:
+    #First read is dummy
+    serial_interface.write(b'a')
+    serial_interface.read()
+    sleep(0.2)
+
+    request_data_from_serial()
+
+    temperature, moisture, watering = serial_interface.readline().decode().strip().split()
+    print(temperature, moisture, watering)
+except:
+    with open(LOG_PATH,'a') as log:
+        log.write(timestamp + ' Unable to retrieve data from serial')
+    exit(1)
+       
+r = requests.post(APP_URL,params = {'temperature':temperature,'moisture':moisture,'watering':watering})
+
+with open(LOG_PATH,'a') as log:
+    log.write(timestamp + ' Temp: ' + str(temperature) + ' Moist: ' + moisture + ' Water: ' + watering + ' Request Status: ' + str(r.status_code) + ' - ' + r.reason + '\n')
